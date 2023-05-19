4edc3e341b3b5f648a31bb0dd86a5c3970378e6a
Timothy AlAzawie <t.alazawie@yahoo.co.uk>
2023-05-19 15:21:02 +0100

    all methods working, needs refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,19 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # index get request
-  get 'tasks', to: 'tasks#index'
+  # get 'tasks', to: 'tasks#index'
+
+  # a get request to create a new task, has to be before show route
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
 
   # show get request
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  #
 
+
+  # delete request to destroy
+  # delete 'task/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
