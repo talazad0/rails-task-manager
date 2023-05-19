7638d740027ae1425f1f3fc735e30ae81c7f3482
Timothy AlAzawie <t.alazawie@yahoo.co.uk>
2023-05-19 13:58:52 +0100

    created show and details page for individual task

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # index get request
+  get 'tasks', to: 'tasks#index'
+
+  # show get request
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  #
+
 end
